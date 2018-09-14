99bc75d1599035341759ad6dfaa9acdbba57800b
Hao <yang.hao0701@gmail.com>
2018-09-14 00:42:12 -0400

    fgdg

--- src/main/java/com/care/CareWeb/TokenController.java
+++ src/main/java/com/care/CareWeb/TokenController.java
@@ -1,16 +1,21 @@
 package com.care.CareWeb;
 
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.*;
 import java.util.concurrent.atomic.AtomicLong;
 
+@SpringBootApplication
 @RestController
 public class TokenController {
     private final AtomicLong counter = new AtomicLong();
 
-    @RequestMapping("/token")
+    public static void main(String[] args) {
+	SpringApplication.run(TokenController.class, args);
+    }
+    
+    @GetMapping("/token")
     public Token token() {
         return new Token(counter.incrementAndGet());
     }
